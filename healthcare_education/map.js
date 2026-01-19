4e264a1192ad803e199fa9d101044f1567c7473d
jgaffuri <jgaf@proton.me>
2026-01-19 16:33:34 +0100

    a

--- healthcare_education/map.js
+++ healthcare_education/map.js
@@ -3,13 +3,7 @@
   add legend circles ?
   */
 
-const urlTiles = "https://ec.europa.eu/assets/estat/E/E4/gisco/accessibility_maps/healthcare_education/tiles/"
-const urlBackground1 = 'https://ec.europa.eu/eurostat/cache/GISCO/mbkg/road/'
-const nuts2jsonURL = "https://ec.europa.eu/assets/estat/E/E4/gisco/pub/nuts2json/v2/";
-const euronymURL = "https://ec.europa.eu/assets/estat/E/E4/gisco/pub/euronym/v3/UTF_LATIN/";
-
 const DEFAULTMAPOSITION = { x: 4030000, y: 2950000, z: 1500 };
-
 const map = new gridviz.Map(document.getElementById('map'), {
     x: DEFAULTMAPOSITION.x,
     y: DEFAULTMAPOSITION.y,
@@ -22,47 +16,19 @@ const preprocess = (c) => {
     c.dt_a3_change = c.dt_a3_2023 == undefined || c.dt_a3_2020 == undefined ? undefined : c.dt_a3_2023 - c.dt_a3_2020
 }
 
-const formatPopulation = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-const dataset = {}
-dataset.education = new gridviz.MultiResolutionDataset(
-    [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000],
-    r => new gviz_par.TiledParquetGrid(map, urlTiles + "education_2025_11/" + r + "/"),
-    { preprocess: preprocess }
-)
-dataset.healthcare = new gridviz.MultiResolutionDataset(
-    [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000],
-    r => new gviz_par.TiledParquetGrid(map, urlTiles + "healthcare_2025_11/" + r + "/"),
-    { preprocess: preprocess }
-)
+const dataset = {
+    education: new gridviz.MultiResolutionDataset(
+        [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000],
+        r => new gviz_par.TiledParquetGrid(map, urlTiles + "education_2025_11/" + r + "/"),
+        { preprocess: preprocess }
+    ),
+    healthcare: new gridviz.MultiResolutionDataset(
+        [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000],
+        r => new gviz_par.TiledParquetGrid(map, urlTiles + "healthcare_2025_11/" + r + "/"),
+        { preprocess: preprocess }
+    ),
+}
 
-//define background layers
-const backgroundLayer1 = new gridviz.BackgroundLayer({
-    url: urlBackground1,
-    //resolutions: Array.from({ length: 12 }, (_, i) => 0.5 * 28.00132289714475 * Math.pow(2, 10 - i)),
-    resolutions: Array.from({ length: 15 }, (_, i) => 114688 / Math.pow(2, i)),
-    origin: [0, 6000000],
-    nbPix: 512, //512 256
-    //filterColor: () => '#ffffff55',
-    visible: (z) => z > 4, //&& z < 2100,
-    pixelationCoefficient: 0.55,
-})
-
-const backgroundLayer2 = new gridviz.BackgroundLayer(
-    //OSMPositronCompositeEN OSMPositronBackground
-    gridviz_eurostat.giscoBackgroundLayer('OSMPositronCompositeEN', 19, 'EPSG3035', {
-        visible: (z) => z <= 4,
-        pixelationCoefficient: 0.55,
-    })
-)
-
-//define boundaries layer
-const boundariesLayer = new gridviz.GeoJSONLayer(gridviz_eurostat.getEurostatBoundariesLayer({ baseURL: nuts2jsonURL, nutsYear: 2024 }))
-//make labels layer
-const labelLayer = new gridviz.LabelLayer(gridviz_eurostat.getEuronymeLabelLayer('EUR', '20', { baseURL: euronymURL, ccIn: ["AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "PL", "PT", "MT", "NL", "RO", "SE", "SK", "SI", "CH", "NO", "LI", "AL",], }))
-
-const strokeStyle = new gridviz.StrokeStyle({ visible: (z) => z < 4 })
-
-const naColor = "#666"
 
 // styles
 const nbClasses = 9
