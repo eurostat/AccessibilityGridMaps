1905daea332aa532773b8876bae09285bc347805
jgaffuri <jgaf@proton.me>
2026-06-22 10:45:40 +0200

    nuts map

--- nuts/ch-map.js
+++ nuts/ch-map.js
@@ -1,3 +1,9 @@
+
+const mapData = {
+    TIME: "2013",
+}
+
+
 export function renderMap(code) {
     console.log(code)
     const isMobile = window.innerWidth <= 768
@@ -85,4 +91,26 @@ export function renderMap(code) {
     //setTimeout(() => console.log(map.position()), 1000)
 }
 
-renderMap('EMP_PLOC_NR')
+//renderMap('EMP_PLOC_NR')
+
+
+
+
+// import the module that registers <ewc-select>
+import './dropdown/ewc-singleselect.js';
+
+// Wait for the custom element to be defined, then initialize
+(async () => {
+    await customElements.whenDefined('ewc-singleselect');
+    // select the element that exists in the page
+    const sel = document.querySelector('#mySelect');
+    if (!sel) return console.warn('#mySelect not found');
+
+    // listen for selection events
+    sel.addEventListener('option-selected', e => {
+        //console.log('option-selected', e.detail);
+        const code = e.detail.option.code;
+        // rebuild everything for the new city
+        renderMap(code);
+    });
+})();
